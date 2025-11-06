f0c934f45cc18340baf5908cfc078244416faed0
dnrdl6828-ai <dnrdl6828@gmail.com>
2025-11-06 07:17:15 +0000

    코드수정2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title("🎈 My new app_붐따붐따붐부루붐붐따")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+    "welcome to boomdda boomdda boomboorooboomboomdda"
 )
